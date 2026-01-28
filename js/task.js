42e32f33f7342cc7360e4c41710dd54f35ad88f4
artyomka <artyomka1350@gmail.com>
2026-01-28 18:34:18 +0300

    Решение задач

--- js/task.js
+++ js/task.js
@@ -6,3 +6,24 @@ let diff = a-b;
 
 console.log(sum)
 console.log(diff)
+
+// ===== Урок 2 =====
+
+let title = "Прочитать конспект";
+
+if(title === "")
+{
+    console.log("Название задачи не указано");
+} else {
+    console.log("Название задачи", title);
+}
+
+let task = 5;
+
+if(task === 0){
+    console.log("Нет задач для выполнения");
+} else if(task <=3 ){
+    console.log("Немного задач");
+} else{
+    console.log("Много задач");
+}
