92b7f6f6699612116493a205bca55a83d833b138
artyomka <artyomka1350@gmail.com>
2026-01-31 15:26:13 +0300

    Стилизация кнопок и выполнена задача: Скидка в мазагине

--- js/task.js
+++ js/task.js
@@ -1,24 +1,65 @@
+// let a=10;
+// let b=5;
+
+// let sum = a+b;
+// let diff = a-b;
+
+// console.log(sum)
+// console.log(diff)
+
+// ===== Урок 2 =====
+
+// let title = "Прочитать конспект";
+
+// if(title === "")
+// {
+//     console.log("Название задачи не указано");
+// } else {
+//     console.log("Название задачи", title);
+// }
+
+// let task = 5;
+
+// if(task === 0){
+//     console.log("Нет задач для выполнения");
+// } else if(task <=3 ){
+//     console.log("Немного задач");
+// } else{
+//     console.log("Много задач");
+// }
+
+    // Домашняя работа
+// 2,3
 let a=10;
 let b=5;
 
 let sum = a+b;
 let diff = a-b;
+let umno = a*b;
+let delenie = a/b;
 
-console.log(sum)
-console.log(diff)
+console.log(sum);
+console.log(diff);
+console.log(umno);
+console.log(delenie);
 
-// ===== Урок 2 =====
+// 4,5
+let name = "Artem";
+let full = "Volokitin";
+let fullname = name + " " + full;
+console.log(fullname);
 
+// 8,9
 let title = "Прочитать конспект";
 
 if(title === "")
 {
     console.log("Название задачи не указано");
 } else {
-    console.log("Название задачи", title);
+    console.log("Задача:", title);
 }
 
-let task = 5;
+let task = 2;
 
 if(task === 0){
     console.log("Нет задач для выполнения");
@@ -26,4 +67,52 @@ if(task === 0){
     console.log("Немного задач");
 } else{
     console.log("Много задач");
+}
+
+// 10
+
+let isCompleted = false;
+
+if(isCompleted === true){
+    console.log("Задача выполена");
+}else  {
+    console.log("Задача ещё в работе");
+}
+
+// 11
+
+let urgent = 23;
+let timebustro = false;
+
+if(urgent === 0) {
+    console.log("Все задачи завершены");
+} else if(timebustro === true) {
+    console.log("Есть срочные задачи");
+} else {
+    console.log("Задачи есть, но они не срочные");
+}
+
+// 12
+
+let isAdmin = true;
+let isModerator = false;
+
+if(isAdmin === true || isModerator === true){
+    console.log("Доступ разрешён");
+}else{
+    console.log("Доступ запрещён");
+}
+
+// Задача "Скидка в магазине"
+
+let amount = 1000;
+
+if(amount === 0){
+    console.log("Корзина пуста");
+}else if( amount < 1000){
+    console.log("Скидка не применяется");
+}else if(amount >= 1000 && amount < 5000){
+    console.log("Скидака 5%");
+}else if(amount > 5000){
+    console.log("Скидка 10%");
 }
