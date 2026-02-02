7193074879edfc3ce390e1ab80ee57b2af28d401
artyomka <artyomka1350@gmail.com>
2026-02-02 18:53:17 +0300

    Стилизация карточек

--- js/task.js
+++ js/task.js
@@ -30,89 +30,114 @@
 
     // Домашняя работа
 // 2,3
-let a=10;
-let b=5;
+// let a=10;
+// let b=5;
 
-let sum = a+b;
-let diff = a-b;
-let umno = a*b;
-let delenie = a/b;
+// let sum = a+b;
+// let diff = a-b;
+// let umno = a*b;
+// let delenie = a/b;
 
-console.log(sum);
-console.log(diff);
-console.log(umno);
-console.log(delenie);
+// console.log(sum);
+// console.log(diff);
+// console.log(umno);
+// console.log(delenie);
 
-// 4,5
-let name = "Artem";
-let full = "Volokitin";
-let fullname = name + " " + full;
-console.log(fullname);
+// // 4,5
+// let name = "Artem";
+// let full = "Volokitin";
+// let fullname = name + " " + full;
+// console.log(fullname);
 
-// 8,9
-let title = "Прочитать конспект";
+// // 8,9
+// let title = "Прочитать конспект";
 
-if(title === "")
+// if(title === "")
+// {
+//     console.log("Название задачи не указано");
+// } else {
+//     console.log("Задача:", title);
+// }
+
+// let task = 2;
+
+// if(task === 0){
+//     console.log("Нет задач для выполнения");
+// } else if(task <=3 ){
+//     console.log("Немного задач");
+// } else{
+//     console.log("Много задач");
+// }
+
+// // 10
+
+// let isCompleted = false;
+
+// if(isCompleted === true){
+//     console.log("Задача выполена");
+// }else  {
+//     console.log("Задача ещё в работе");
+// }
+
+// // 11
+
+// let urgent = 23;
+// let timebustro = false;
+
+// if(urgent === 0) {
+//     console.log("Все задачи завершены");
+// } else if(timebustro === true) {
+//     console.log("Есть срочные задачи");
+// } else {
+//     console.log("Задачи есть, но они не срочные");
+// }
+
+// // 12
+
+// let isAdmin = true;
+// let isModerator = false;
+
+// if(isAdmin === true || isModerator === true){
+//     console.log("Доступ разрешён");
+// }else{
+//     console.log("Доступ запрещён");
+// }
+
+// // Задача "Скидка в магазине"
+
+// let amount = 1000;
+
+// if(amount === 0){
+//     console.log("Корзина пуста");
+// }else if( amount < 1000){
+//     console.log("Скидка не применяется");
+// }else if(amount >= 1000 && amount < 5000){
+//     console.log("Скидака 5%");
+// }else if(amount > 5000){
+//     console.log("Скидка 10%");
+// }
+
+        // УРОК 3
+
+function sum(a, b ){
+    return a + b;
+}
+let result = sum(10, 15);
+console.log(sum(2,3));
+
+function isTaskDone(status)
 {
-    console.log("Название задачи не указано");
-} else {
-    console.log("Задача:", title);
+    return status === "Выполненно";
 }
+console.log(isTaskDone("Выполненно"));
 
-let task = 2;
+let done = isTaskDone("В процессе");
+console.log(done);
 
-if(task === 0){
-    console.log("Нет задач для выполнения");
-} else if(task <=3 ){
-    console.log("Немного задач");
-} else{
-    console.log("Много задач");
+
+function taskSummary(total, done)
+{
+    const active = total - done;
+    console.log("Всего: " + total + " Активных: " + active + " Выполнено: " + done);
 }
-
-// 10
-
-let isCompleted = false;
-
-if(isCompleted === true){
-    console.log("Задача выполена");
-}else  {
-    console.log("Задача ещё в работе");
-}
-
-// 11
-
-let urgent = 23;
-let timebustro = false;
-
-if(urgent === 0) {
-    console.log("Все задачи завершены");
-} else if(timebustro === true) {
-    console.log("Есть срочные задачи");
-} else {
-    console.log("Задачи есть, но они не срочные");
-}
-
-// 12
-
-let isAdmin = true;
-let isModerator = false;
-
-if(isAdmin === true || isModerator === true){
-    console.log("Доступ разрешён");
-}else{
-    console.log("Доступ запрещён");
-}
-
-// Задача "Скидка в магазине"
-
-let amount = 1000;
-
-if(amount === 0){
-    console.log("Корзина пуста");
-}else if( amount < 1000){
-    console.log("Скидка не применяется");
-}else if(amount >= 1000 && amount < 5000){
-    console.log("Скидака 5%");
-}else if(amount > 5000){
-    console.log("Скидка 10%");
-}
+taskSummary(10,4);
