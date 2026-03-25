b9d32388354bca89603f7782e53254db19bbdc71
artyomka <artyomka1350@gmail.com>
2026-03-25 18:53:16 +0300

    Все сломалось :(

---
+++ js/kanban.js
@@ -0,0 +1,54 @@
+const columns = document.querySelectorAll(".column");
+
+let boardData = JSON.parse(localStorage.getItem("kanbanData")) || {
+  todo: [],
+  "in-prog": [],
+  done: [],
+};
+
+function renderBoard() {
+  columns.forEach((column) => {
+    const status = column.dataset.status;
+    const taskList = column.querySelector(".column__tasks");
+
+    taskList.innerHTML = "";
+
+    boardData[status].forEach((task, index) => {
+      const el = document.createElement("div");
+
+      el.className = "task-kanban";
+      el.dataset.index = index;
+
+      el.innerHTML = `
+            <h3 class="task-kanban__title">${task.title}</h3>
+              ${
+                task.descr
+                  ? `<p class="task-kanban__descr">
+                ${task.descr}
+              </p>`
+                  : ""
+              }
+
+              <div class="task-kanban__footer">
+                <span class="task-kanban__label ${task.priority}">${task.priority}</span>
+                <span class="task-kanban__date">${task.deadline}</span>
+              </div>
+            `;
+
+        taskList.appendChild(el);
+    });
+
+    updateCount(column)
+  });
+
+  localStorage.setItem("kanbanData", JSON.stringify(boardData))
+}
+
+function updateCount(column) {
+    const countEL = column.querySelector(".column__count");
+    const status = column.dataset.status;
+
+    countEL.textContent = boardData[status].lenght;
+}
+
+renderBoard()
